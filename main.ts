06c7dba870364624a0a310deadd45f57579abf81
RJWPod <101339801+RJWPod@users.noreply.github.com>
2022-05-09 09:54:40 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,17 @@
 input.onButtonPressed(Button.A, function () {
-	
+    Ambulance += 1
+    if (Ambulance == 1) {
+        radio.sendNumber(7)
+    }
+    Ambulance = 0
 })
+let Ambulance = 0
+basic.showLeds(`
+    . . . . .
+    . # # # .
+    # # # # #
+    . # . # .
+    . . . . .
+    `)
 radio.setGroup(177)
-basic.forever(function () {
-	
-})
+Ambulance = 0
